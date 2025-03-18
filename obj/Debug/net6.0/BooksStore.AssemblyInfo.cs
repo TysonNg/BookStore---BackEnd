5db392c586d742c44ff3cdade4e4041b39281255
NGUYEN MINH NHAT <nhatnguyenminh1507@gmail.com>
2025-03-18 18:30:33 +0700

    update
    update

--- obj/Debug/net6.0/BooksStore.AssemblyInfo.cs
+++ obj/Debug/net6.0/BooksStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd6080cd393aa0c103a32b361342b68d58ba5488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719569283954a3ad46f648fcf692e8569b6622b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
